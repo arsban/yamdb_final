6cccd0de8e1ac6a6c1a258fe739475eef288f8d7
Георгий <georggeorguy@gmail.com>
2022-01-23 14:48:00 +0300

    var_str_5.2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -29,8 +29,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
     'django_filters',
+    
     'api',
     'reviews',
 ]
